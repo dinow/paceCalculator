7f2719e48c1f0403955f51a22bbeda321fdebc0c
Didier Nowak <didiernowak@gmail.com>
2013-10-16 21:23:14 +0200

    multi pages

--- gen/be/dno/running/pacecalculator/R.java
+++ gen/be/dno/running/pacecalculator/R.java
@@ -24,25 +24,30 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f05000f;
+        public static final int action_settings=0x7f050010;
         public static final int backbutton=0x7f050000;
         public static final int btnResetAllure=0x7f050008;
         public static final int btnResetKms=0x7f050001;
-        public static final int btnResetPurcVMA=0x7f05000a;
+        public static final int btnResetPurcVMA=0x7f05000b;
         public static final int btnResetTemps=0x7f050003;
-        public static final int btnResetVMA=0x7f05000c;
+        public static final int btnResetVMA=0x7f05000d;
         public static final int btnResetVitesse=0x7f050009;
         public static final int calcul=0x7f050005;
-        public static final int helperText=0x7f05000e;
+        public static final int helperText=0x7f05000f;
+        public static final int mypanelpager=0x7f05000a;
         public static final int txtAllure=0x7f050006;
         public static final int txtKms=0x7f050002;
-        public static final int txtPourcVMA=0x7f05000b;
+        public static final int txtPourcVMA=0x7f05000c;
         public static final int txtTemps=0x7f050004;
-        public static final int txtVMA=0x7f05000d;
+        public static final int txtVMA=0x7f05000e;
         public static final int txtVitesse=0x7f050007;
     }
     public static final class layout {
         public static final int activity_pace_calculator=0x7f030000;
+        public static final int pace_calc=0x7f030001;
+        public static final int previsions=0x7f030002;
+        public static final int specifique=0x7f030003;
+        public static final int vma=0x7f030004;
     }
     public static final class menu {
         public static final int pace_calculator=0x7f080000;
--- src/be/dno/running/pacecalculator/PaceCalculator.java
+++ src/be/dno/running/pacecalculator/PaceCalculator.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -15,17 +19,25 @@ public class PaceCalculator extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pace_calculator);
+		
+
+        MyPagerAdapter adapter = new MyPagerAdapter();
+        ViewPager myPager = (ViewPager) findViewById(R.id.mypanelpager);
+        myPager.setAdapter(adapter);
+        myPager.setCurrentItem(0);
+     
+		
+	}
+	
+	private void loadVMACalc(){
 		TextView helperText = 	(TextView)findViewById(R.id.helperText);
 		EditText etVMA = 		(EditText)findViewById(R.id.txtVMA);
-		
-		//load VMA
 		try{ 
 			SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
 			etVMA.setText(sharedPref.getString("vma", ""));
 		}catch(Exception ex){
 			helperText.setText(ex.getMessage());
 		}
-		
 	}
 
 	@Override
@@ -124,4 +136,79 @@ public class PaceCalculator extends Activity {
 	public void resetPourcVMA(View paramView){
 		((EditText)findViewById(R.id.txtPourcVMA)).setText("");
 	}
+	
+	private class MyPagerAdapter extends PagerAdapter {
+
+        public int getCount() {
+                return 4;
+        }
+
+        public Object instantiateItem(View collection, int position) {
+
+                LayoutInflater inflater = (LayoutInflater) collection.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                int resId = 0;
+                switch (position) {
+	                case 0:
+	                        resId = R.layout.pace_calc;
+	                        
+	                        break;
+	                case 1:
+	                        resId = R.layout.vma;
+	                        break;
+	                case 2:
+	                        resId = R.layout.specifique;
+	                        break;
+	                case 3:
+	                        resId = R.layout.previsions;
+	                        break;
+                }
+
+                View view = inflater.inflate(resId, null);
+
+                ((ViewPager) collection).addView(view, 0);
+                if (resId == R.layout.pace_calc){
+                	loadVMACalc();
+                }
+                
+                return view;
+        }
+
+        @Override
+        public void destroyItem(View arg0, int arg1, Object arg2) {
+                ((ViewPager) arg0).removeView((View) arg2);
+
+        }
+
+        @Override
+        public void finishUpdate(View arg0) {
+                // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+                return arg0 == ((View) arg1);
+
+        }
+
+        @Override
+        public void restoreState(Parcelable arg0, ClassLoader arg1) {
+                // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public Parcelable saveState() {
+                // TODO Auto-generated method stub
+                return null;
+        }
+
+        @Override
+        public void startUpdate(View arg0) {
+                // TODO Auto-generated method stub
+
+        }
+
+}
 }
